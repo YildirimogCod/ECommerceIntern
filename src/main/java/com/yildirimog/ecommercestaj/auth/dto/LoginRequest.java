e8d7ecb5c5507e0523051dea73c3a6eb4d4ce6b4
yildirimogcod <yildirimogcod@gmail.com>
2025-07-07 15:03:19 +0300

    validation chechked

--- src/main/java/com/yildirimog/ecommercestaj/auth/dto/LoginRequest.java
+++ src/main/java/com/yildirimog/ecommercestaj/auth/dto/LoginRequest.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
         @Email(message = "Geçerli bir email giriniz")
+
         @NotBlank(message = "Email alanı boş olamaz")
         String email,
         @NotBlank(message = "Şifre alanı boş olamaz")
         String password
+
 ) {
 }
